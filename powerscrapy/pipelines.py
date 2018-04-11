37a1e7396b0a3c1a1dcfe6cd9fcc514593761486
KoiCarrot <402704088@qq.com>
2018-04-11 21:54:05 +0800

    second commit

--- powerscrapy/pipelines.py
+++ powerscrapy/pipelines.py
@@ -4,8 +4,21 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import pymysql.cursors
 
 class PowerscrapyPipeline(object):
+    def __init__(self):
+        self.connect = pymysql.connect(
+            host = '127.0.0.1',
+            port = 3306,
+            db = 'powerscrapy',
+            user = 'root',
+            passwd = '',
+            charset = 'utf8',
+            use_unicode = True
+        )
+        self.cursor = self.connect.cursor()
     def process_item(self, item, spider):
+        self.cursor.execute('insert into Yibuyy(title,url)values(%s,%s)',(item['title'],item['url']))
+        self.connect.commit()
         return item
